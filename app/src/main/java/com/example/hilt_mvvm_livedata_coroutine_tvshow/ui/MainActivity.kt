73714780d8b5436bfb298b264e7eb437a1d782b9
mosayeb <mb.masoumi1000@gmail.com>
2022-07-09 17:05:12 +0430

    resolve multiple trig

--- app/src/main/java/com/example/hilt_mvvm_livedata_coroutine_tvshow/ui/MainActivity.kt
+++ app/src/main/java/com/example/hilt_mvvm_livedata_coroutine_tvshow/ui/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.hilt_mvvm_livedata_coroutine_tvshow.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import com.bersyte.tvshowapp.models.UserItemModel
 import com.example.hilt_mvvm_livedata_coroutine_tvshow.R
 import com.example.hilt_mvvm_livedata_coroutine_tvshow.resource.Resource
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 @AndroidEntryPoint
@@ -29,38 +29,56 @@ class MainActivity : AppCompatActivity() {
 
         btn_get_data.setOnClickListener {
 
-
             mainViewModel.getTvShowList.observe(this) {
-                if(it.status == Resource.Status.SUCCESS){
+                if (it.status == Resource.Status.SUCCESS) {
+                    mainViewModel.getTvShowList.removeObservers(this)
                     val result = it.data
                     val size = result?.size.toString()
-                    txt.text = size +" / "+ result?.get(0)?.title
+                    txt.text = size + " / " + result?.get(0)?.title
 
                     mainViewModel.getTvShowList.removeObservers(this)
 
-                }else if(it.status == Resource.Status.ERROR){
+                } else if (it.status == Resource.Status.ERROR) {
                     txt.text = it.message
 
-                }else if(it.status == Resource.Status.LOADING){
+                } else if (it.status == Resource.Status.LOADING) {
                     txt.text = "loading..."
                 }
             }
 
-//            mainViewModel.getTvShowList.observeForever() {
-//                if (it.status == Resource.Status.SUCCESS) {
-//                    val result = it.data
-//                    val size = result?.size.toString()
-//                    txt.text = size + " / " + result?.get(0)?.title
-//                } else if (it.status == Resource.Status.ERROR) {
-//                    txt.text = it.message
-//
-//                } else if (it.status == Resource.Status.LOADING) {
-//                    txt.text = "loading..."
-//                }
-//            }
-
 
 
         }
+
+
+
+        /***************************************** correct implementation*********************/
+        var item = -1
+
+        btn_get_item.setOnClickListener {
+            item++
+            val getItemLiveData = mainViewModel.getItem(item)
+            getItemLiveData.observe(this) {
+
+
+                if(it !=null){
+                    if (it.status == Resource.Status.SUCCESS) {
+                        val result = it.data
+                        txt.text = result?.title
+                        getItemLiveData.removeObservers(this)
+                        mainViewModel.onCleared()  // to prevent looping
+
+                    } else if (it.status == Resource.Status.ERROR) {
+                        txt.text = it.message
+                        mainViewModel.onCleared()  // to prevent using cache looping
+                    } else if (it.status == Resource.Status.LOADING) {
+                        txt.text = "loading..."
+                    }
+                }
+            }
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/hilt_mvvm_livedata_coroutine_tvshow/ui/MainViewModel.kt
+++ app/src/main/java/com/example/hilt_mvvm_livedata_coroutine_tvshow/ui/MainViewModel.kt
@@ -1,8 +1,5 @@
 package com.example.hilt_mvvm_livedata_coroutine_tvshow.ui
 
-import android.util.Log
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.*
 import com.bersyte.tvshowapp.models.UserItemModel
 import com.example.hilt_mvvm_livedata_coroutine_tvshow.resource.Resource
@@ -13,49 +10,73 @@ import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel(){
+class MainViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel() {
 
 
-    private val _response = MutableLiveData<Resource<List<UserItemModel>>>()
-    val getTvShowList: MutableLiveData<Resource<List<UserItemModel>>> get() = _response
+    private val _responseList = MutableLiveData<Resource<List<UserItemModel>>>()
+    val getTvShowList: MutableLiveData<Resource<List<UserItemModel>>> get() = _responseList
 
 
-//    private val _response = mutableStateOf(UserListState())
-//    val getTvShowList : State<UserListState> = _response
+
+    val getItemLiveData: MutableLiveData<Resource<UserItemModel>> = MutableLiveData<Resource<UserItemModel>>()
+
+
 
 
     init {
         getAllTvShows()
-        _response.value = Resource.Loading()
+        _responseList.value = Resource.Loading()
     }
+
 
     private fun getAllTvShows() = viewModelScope.launch {
-
         try {
-
             mainRepository.getShowTvList().let { response ->
-
                 if (response.isSuccessful) {
-//                  _response.value = UserListState(userList = response.body())
-                    _response.value = Resource.Success(response.body())
-
-
-                }else{
-                    _response.value = Resource.Error( response.message(), null)
+                    _responseList.value = Resource.Success(response.body())
+                } else {
+                    _responseList.value = Resource.Error(response.message(), null)
                 }
-
             }
-
-        }catch (e: HttpException){
-            _response.value = Resource.Error(e.localizedMessage ?: "an unexpected error occured", null)
-        }catch (e: IOException){
-            _response.value = Resource.Error( "check your internet connection", null)
+        } catch (e: HttpException) {
+            _responseList.value =
+                Resource.Error(e.localizedMessage ?: "an unexpected error occured", null)
+        } catch (e: IOException) {
+            _responseList.value = Resource.Error("check your internet connection", null)
         }
-
-
-
-
     }
 
 
+    fun getItem(user_id: Int): LiveData<Resource<UserItemModel>?> {
+
+
+        getItemLiveData.value = Resource.Loading()  // to show loading when click on button
+
+        viewModelScope.launch {
+            try {
+                mainRepository.getShowTvList().let { response ->
+                    if (response.isSuccessful) {
+                        getItemLiveData.value = Resource.Success(response.body()?.get(user_id))
+                    } else {
+                        getItemLiveData.value = Resource.Error(response.message(), null)
+                    }
+                }
+            } catch (e: HttpException) {
+                getItemLiveData.value = Resource.Error(e.localizedMessage ?: "an unexpected error occured", null)
+            } catch (e: IOException) {
+                getItemLiveData.value = Resource.Error("check your internet connection", null)
+            }
+        }
+
+        return getItemLiveData
+    }
+
+
+
+
+    public override fun onCleared() {
+        getItemLiveData.value = null
+    }
+
 }
+
